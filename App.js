44b4b2e3e9d5995aee644aca29e135c9420b1f3f
evanderson vasconcelos <evandersonvasconcelos23@gmail.com>
2020-10-25 21:58:14 -0300

    Adding expo-fonts and config

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
+import {
+  Nunito_400Regular,
+  Nunito_700Bold,
+  Nunito_800ExtraBold,
+  useFonts,
+} from '@expo-google-fonts/nunito'
+import { AppLoading } from 'expo'
+
 import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_400Regular,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
